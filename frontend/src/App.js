5d664ed02320dd1a8c22cc2e8f182b183a7c2e62
Joulence <4erviken@gmail.com>
2023-05-28 17:51:35 +0300

    links receive an "active" class when active

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
