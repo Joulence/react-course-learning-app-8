d85a5f97819eb2f48c55d7606534dcab3e19de16
Joulence <4erviken@gmail.com>
2023-05-28 19:12:05 +0300

    The json() Utility Function

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -9,7 +9,7 @@ function ErrorPage() {
     let message = 'Something went wrong!';
 
     if (error.status === 500) {
-        message = JSON.parse(error.data).message;
+        message = error.data.message;
     }
     if (error.status === 404) {
         title = 'Not found!';
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom';
+import { json, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
@@ -15,16 +15,22 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-    const response = await fetch('http://localhost:8080/eventsdsd');
+    const response = await fetch('http://localhost:8080/events');
     if (!response.ok) {
 
-        throw new Response(JSON.stringify(
-            {
-                message: 'Could not fetch events!'
-            }),
+        // throw new Response(JSON.stringify(
+        //     {
+        //         message: 'Could not fetch events!'
+        //     }),
+        //     {
+        //         status: 500
+        //     });
+
+        throw json({ message: 'Could not fetch events!' },
             {
                 status: 500
-            });
+            }
+        );
     } else {
         return response;
     }
