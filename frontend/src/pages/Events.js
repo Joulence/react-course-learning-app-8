070f6652f690ac1397eba42bcb6529ed06f049bc
Joulence <4erviken@gmail.com>
2023-05-28 18:23:36 +0300

    Data fetching with the loader

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,33 +1,14 @@
-import { Link } from "react-router-dom";
-
-const DUMMY_EVENTS = [
-    {
-        id: 'e1',
-        title: 'some event'
-    },
-    {
-        id: 'e2',
-        title: '2 some event'
-    },
-    {
-        id: 'e3',
-        title: ' 3some event'
-    },
-];
+import { useLoaderData } from 'react-router-dom';
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
+
+    const events = useLoaderData();
     return (
         <>
-            <h1>Events Page</h1>
-            <ul>
-                {DUMMY_EVENTS.map((event) => (
-                    <li key={event.id}>
-                        <Link to={event.id}>{event.title}</Link>
-                    </li>
-                ))}
-            </ul>
+            <EventsList events={events} />
         </>
     );
-};
+}
 
 export default EventsPage;
