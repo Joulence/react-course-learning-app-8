63fc18fc5c1af2bfd6a466330b8cb1bfd952417f
Joulence <4erviken@gmail.com>
2023-05-28 18:02:23 +0300

    Ready Template using Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,3 @@
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import {
   createBrowserRouter,
   RouterProvider
@@ -14,6 +9,7 @@ import EventDetailPage from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootLayout from './pages/Root';
+import EventsRootLayout from './pages/EventsRoot';
 
 const router = createBrowserRouter([
   {
@@ -21,10 +17,16 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'events', element: <EventsPage /> },
-      { path: 'events/:eventId', element: <EventDetailPage /> },
-      { path: 'events/new', element: <NewEventPage /> },
-      { path: 'events/:eventId/edit', element: <EditEventPage /> },
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          { index: true, element: <EventsPage /> },
+          { path: ':eventId', element: <EventDetailPage /> },
+          { path: 'new', element: <NewEventPage /> },
+          { path: ':eventId/edit', element: <EditEventPage /> },
+        ]
+      }
     ]
   }
 
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,6 +1,32 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+    {
+        id: 'e1',
+        title: 'some event'
+    },
+    {
+        id: 'e2',
+        title: '2 some event'
+    },
+    {
+        id: 'e3',
+        title: ' 3some event'
+    },
+];
+
 function EventsPage() {
     return (
-        <h1>Events Page</h1>
+        <>
+            <h1>Events Page</h1>
+            <ul>
+                {DUMMY_EVENTS.map((event) => (
+                    <li key={event.id}>
+                        <Link to={event.id}>{event.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 };
 
