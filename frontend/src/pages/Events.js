980885129e2c027cdaef31095250b65de9675c40
Joulence <4erviken@gmail.com>
2023-05-28 18:42:10 +0300

    Returning Responses in loader

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -2,8 +2,8 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-
-    const events = useLoaderData();
+    const data = useLoaderData();
+    const events = data.events;
     return (
         <>
             <EventsList events={events} />
@@ -18,7 +18,6 @@ export async function loader() {
     if (!response.ok) {
         //...
     } else {
-        const resData = await response.json();
-        return resData.events;
+        return response;
     }
 }
