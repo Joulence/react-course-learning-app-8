b2dca6206e753f390421061f532c543f6be37682
Joulence <4erviken@gmail.com>
2023-05-29 18:57:04 +0300

    Working with action() Functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import {
 import HomePage from './pages/Home';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetail';
-import NewEventPage from './pages/NewEvent';
+import NewEventPage, { action as newEventAction } from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootLayout from './pages/Root';
 import EventsRootLayout from './pages/EventsRoot';
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
               { path: 'edit', element: <EditEventPage /> },
             ]
           },
-          { path: 'new', element: <NewEventPage /> },
+          { path: 'new', element: <NewEventPage />, action: newEventAction },
         ]
       }
     ]
