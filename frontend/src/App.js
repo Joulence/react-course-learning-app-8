1a5f03b64b7db2a414ac70fcfa2ce1d2f372d629
Joulence <4erviken@gmail.com>
2023-05-29 19:17:18 +0300

    Submitting Data Programmatically

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,10 @@ import {
 
 import HomePage from './pages/Home';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetail';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction
+} from './pages/EventDetail';
 import NewEventPage, { action as newEventAction } from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootLayout from './pages/Root';
@@ -33,7 +36,11 @@ const router = createBrowserRouter([
             id: 'event-detail',
             loader: eventDetailLoader,
             children: [
-              { index: true, element: <EventDetailPage />, },
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction
+              },
               { path: 'edit', element: <EditEventPage /> },
             ]
           },
