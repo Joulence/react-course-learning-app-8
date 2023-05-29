dcd350b9b45f52ef5c668bd04b3f5c51c02e1010
Joulence <4erviken@gmail.com>
2023-05-29 18:19:18 +0300

    Accessing Data From Other Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,9 +28,16 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader
           },
-          { path: ':eventId', element: <EventDetailPage />, loader: eventDetailLoader },
+          {
+            path: ':eventId',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
+              { index: true, element: <EventDetailPage />, },
+              { path: 'edit', element: <EditEventPage /> },
+            ]
+          },
           { path: 'new', element: <NewEventPage /> },
-          { path: ':eventId/edit', element: <EditEventPage /> },
         ]
       }
     ]
--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -1,6 +1,12 @@
+import { useRouteLoaderData } from 'react-router-dom';
+import EventForm from '../components/EventForm';
+
 function EditEventPage() {
+    const data = useRouteLoaderData('event-detail');
     return (
-        <h1>Edit Event Page</h1>
+        <>
+            <EventForm event={data.event}/>
+        </>
     );
 };
 
