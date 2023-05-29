818ece02eba21f20c85d78235f062576dc05537c
Joulence <4erviken@gmail.com>
2023-05-29 20:35:15 +0300

    Home Page updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,10 @@
+import PageContent from '../components/PageContent';
+
 function HomePage() {
     return (
-        <h1>Home Page</h1>
+        <PageContent title="Welcome!">
+            <p>Browse all our amazing events</p>
+        </PageContent>
     );
 };
 
